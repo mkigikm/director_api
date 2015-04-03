b4ff0a6585548015ff100d0461bc9ce10751299f
Matthew Hicks <mkigikm@gmail.com>
2015-04-03 13:50:46 -0400

    adds director model and test and methods for retrieving livestream data

---
+++ app/models/director.js
@@ -0,0 +1,28 @@
+var request = require('request');
+
+var apiURL = 'https://api.new.livestream.com/accounts/';
+
+var Director = function (livestreamId) {
+  this.id = livestreamId;
+  this.fields = {livestream_id: livestreamId};
+}
+
+Director.prototype.getLivestreamFields = function (callback) {
+  request(this.url(), function (err, res, body) {
+    var ok = !err && res.statusCode === 200;
+    
+    if (ok) {
+      body = JSON.parse(body);
+      this.fields.full_name = body.full_name;
+      this.fields.dob       = body.dob;
+    }
+
+    callback(ok);
+  }.bind(this));
+};
+
+Director.prototype.url = function () {
+  return apiURL + this.id;
+};
+
+module.exports = Director;
---
+++ test/models/director.js
@@ -0,0 +1,25 @@
+var should = require('should');
+var Director = require('../../app/models/director');
+
+it('retrieves director data', function (done) {
+  var cameron = new Director('6488824');
+
+  cameron.getLivestreamFields(function (ok) {
+    (cameron.full_name).should.be.exactly('James Cameron')
+    ok.should.be.true;
+  });
+
+  done();
+});
+
+it('sets nothing when director is not found', function (done) {
+  var nowhereMan = new Director('foo');
+
+  nowhereMan.getLivestreamFields(function (ok) {
+    nowhereMan.should.not.have.property('full_name');
+    nowhereMan.should.not.have.property('dob');
+    ok.should.be.false;
+  });
+
+  done();
+});
